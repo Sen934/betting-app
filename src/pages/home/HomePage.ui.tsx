aced055aafd912bd68475d1a7a77b21007a1f713
Sen934 <46794874+Sen934@users.noreply.github.com>
2025-05-07 12:15:30 +0200

    Ad MUI, set up GamesDashboard widget

--- src/pages/home/HomePage.ui.tsx
+++ src/pages/home/HomePage.ui.tsx
@@ -1,3 +1,24 @@
+import CasinoOutlinedIcon from '@mui/icons-material/CasinoOutlined';
+import { Box, Typography } from '@mui/material';
+import { GamesDashboard } from '@/widgets/games-dashboard/GamesDashboard.ui.tsx';
+
 export default function HomePage() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        gap="4px"
+        component="header"
+      >
+        <Typography variant="h3">BET</Typography>
+        <CasinoOutlinedIcon fontSize="large" />
+      </Box>
+      <main>
+        <GamesDashboard />
+      </main>
+      <footer>my social</footer>
+    </>
+  );
 }
---
+++ src/widgets/games-dashboard/GamesDashboard.ui.tsx
@@ -0,0 +1,1 @@
+export const GamesDashboard = () => <div>Dashboard</div>;
